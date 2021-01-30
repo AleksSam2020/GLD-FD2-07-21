44015e785207a2e46c835329a74a13fb5830ea7c
Samodumova <sanyasam2020@gmail.com>
2021-01-30 19:33:31 +0300

    done tasks

---
+++ js/variables/task-1-2-3.js
@@ -0,0 +1,30 @@
+// task-1
+(function () {
+  let days = 4;
+  let countSecondsInDays = days * 24 * 60 * 60;
+
+  console.log(countSecondsInDays);
+})();
+
+// task-2
+(function () {
+  let admin;
+  let name;
+
+  name = 'Aleksandra';
+  admin = name;
+  console.log(admin);
+})();
+
+//task-3
+(function () {
+  let hour;
+  let minute;
+  let second;
+
+  hour = 10;
+  minute = 40;
+  second = 25;
+
+  console.log(`${hour}:${minute}:${second}`)
+})();
