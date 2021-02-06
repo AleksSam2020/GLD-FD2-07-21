f9688611b6705c9d14fe7082c015e58c329a8495
Samodumova <sanyasam2020@gmail.com>
2021-02-06 18:33:07 +0300

    strings

---
+++ js/strings/task-6.js
@@ -0,0 +1,16 @@
+/*Проверить, содержит ли строка второе вхождение подстроки, вернуть true или false. Используйте String.prototype.includes(), String.prototype.indexOf()*/
+
+let str = 'I learn Javascript every day';
+let count = 0;
+
+for(i = 0; i < str.length; i++) {
+  if (str[i].includes('a')) {
+    count ++;
+  }
+}
+
+if(count >= 2) {
+  console.log(true);
+} else {
+  console.log(false);
+}
---
+++ js/strings/task-8.js
@@ -0,0 +1,11 @@
+/*Создать строку: 10 times 2 is 20' используя переменные:
+const a = 10;
+const b = 2;
+Используйте шаблонные строки.*/
+
+const a = 10;
+const b = 2;
+
+let str = `${a} times ${b} is 20`;
+
+console.log(str);
