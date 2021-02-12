598da142876c5b7da1c02c55e9e5ec135dd2f4d6
Samodumova <sanyasam2020@gmail.com>
2021-02-12 18:37:39 +0300

    functions-basics

--- js/functions/basics/task-8.js
+++ js/functions/basics/task-8.js
@@ -1,22 +1,9 @@
 /*Создайте конструкцию, с помощью которой сразу выполниться выше реализованная функция conc. Используйте IIFE.*/
 (function() {
-  let arr1 = [1, 8, 3, 5, 12, 7, 9, 11];
-  let arr2 = [];
-
-  let func = (a, b) => {
-    for(let i = 0; i < a.length; i++) {
-      if(a[i] * a[i] < 100) {
-      b.push(a[i] * a[i]);
-      }
-    }
-  };
-
-  (func(arr1, arr2))
-
-
-  arr2.sort(function(a, b) {
-    return a - b;
-  });
-
-  console.log(arr2);
+  console.log(conc('1', '1'));
+  console.log(conc(1, 1));
+  
+  function conc (a, b) {
+    return '' + a + b;
+  }
 })()
